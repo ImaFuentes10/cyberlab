e4418e1e4dbc4ec8e10c7e83f41695423d7ef4eb
ImaFuentes10 <ima.fuentes.c@gmail.com>
2025-10-20 16:54:29 -0700

    Agrego URL para PDF

--- cyberlab/src/App.jsx
+++ cyberlab/src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Pdf from './pages/Pdf'
+import { Routes, Route, Navigate, BrowserRouter } from 'react-router-dom'
+import Pdf from './pages/Pdf.jsx'
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path='/' element={<Navigate to='/CyberLab/omedec/p.php?s=tEQTdUUdTvW1cbDMZvfn%252BtzA9l84SZAGdkx5sZgtJ2c%253D' replace />} />
-        <Route path='/CyberLab/omedec/p.php?s=tEQTdUUdTvW1cbDMZvfn%252BtzA9l84SZAGdkx5sZgtJ2c%253D' element={<Pdf />} />
-      </Routes>
-    </div>
+    <main>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' 
+          element={<Navigate to='/CyberLab/omedec/p.php?s=tEQTdUUdTvW1cbDMZvfn%252BtzA9l84SZAGdkx5sZgtJ2c%253D' replace />} />
+          <Route path='/CyberLab/omedec/p.php' element={<Pdf /> } />
+        </Routes>
+      </BrowserRouter>
+    </main>
   )
 }
 
--- cyberlab/src/pages/Pdf.jsx
+++ cyberlab/src/pages/Pdf.jsx
@@ -1,13 +1,15 @@
+import pruebaPdf from '../assets/prueba.pdf'
+
 function Pdf() {
   return (
     <div className="">
-        <embed
-            src="../assets/prueba.pdf"
-            type="application/pdf"
-            width="100%"
-            height="1000"
-            title="Embedded PDF Viewer"
-        />
+      <embed
+        src={pruebaPdf}
+        type="application/pdf"
+        width="100%"
+        height="1000"
+        title="Embedded PDF Viewer"
+      />
     </div>
   )
 }
