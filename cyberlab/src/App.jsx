c640a637da4e0f82744a5cb2af03dcbbe5176235
ImaFuentes10 <ima.fuentes.c@gmail.com>
2025-10-20 17:21:46 -0700

    cambios a rutas sugeridos por chatgpt

--- cyberlab/src/App.jsx
+++ cyberlab/src/App.jsx
@@ -4,16 +4,24 @@ import Pdf from './pages/pdf.jsx'
 
 function App() {
   return (
-    <main>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' 
-          element={<Navigate to='/CyberLab/omedec/p.php?s=tEQTdUUdTvW1cbDMZvfn%252BtzA9l84SZAGdkx5sZgtJ2c%253D' replace />} />
-          <Route path='/CyberLab/omedec/p.php' element={<Pdf /> } />
-        </Routes>
-      </BrowserRouter>
-    </main>
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* Redirige de "/" a "/CyberLab/omedec/p.php?s=..." */}
+        <Route
+          path="/"
+          element={
+            <Navigate
+              to="/CyberLab/omedec/p.php?s=tEQTdUUdTvW1cbDMZvfn%252BtzA9l84SZAGdkx5sZgtJ2c%253D"
+              replace
+            />
+          }
+        />
+
+        {/* Ruta destino */}
+        <Route path="/CyberLab/omedec/p.php" element={<Pdf />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
